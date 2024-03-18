072f65a4d9e59f9c343c56f546912f12609e3e74
Sleepyy-17 <laillier.alexis2004@gmail.com>
2024-03-18 14:51:22 +0100

    - Ajout de la navbar (pc et mobile) - Ajout d'une font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Nav from "@/components/nav";
+import MobileNav from "@/components/mobileNav";
 
 const HomePage = () => {
-    return <div>Home Page</div>;
+  const [nav, setNav] = React.useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav}/>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
